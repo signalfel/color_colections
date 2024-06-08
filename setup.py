8b30b042debb3ee04224ea527a44687a6f2472ca
Nils @ Team Analytics <nodelynk@gmail.com>
2024-06-08 13:50:41 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='color_collections',
+      version='1.0',
+      author='signalfel',
+      author_email='nodelynk@gmail.com',
+      py_modules=['palettes'],
+      )
