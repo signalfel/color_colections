ef84d7cec578e21836e1caaf93c0b994883044eb
Nils @ Team Analytics <nodelynk@gmail.com>
2024-06-08 13:50:21 +0200

    Create palettes.py

---
+++ palettes.py
@@ -0,0 +1,6 @@
+eljest = {'natt': '#0E171B',
+          #'kotte': '#422A25',
+          'kotte': '#4E322C',
+          'barr': '#50933B',
+          'skymning': '#3B7C93',
+          'gryning': '#BD6D24'}
